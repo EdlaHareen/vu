18e8a5ff26fbc902c915a67084903ea5ffd5bc6c
Narsimhareddy28 <k.sainarsimhareddy03@gmail.com>
2023-12-07 19:43:34 -0600

    deploy

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ npm run build
 git init
 git add -A
 git commit -m 'deploy'
-
+git remote set-url origin https://github.com/EdlaHareen/vue.git
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
