f15ed63bd31f9234979757eef96a1b51ddde2ce5
Narsimhareddy28 <k.sainarsimhareddy03@gmail.com>
2023-12-07 19:13:35 -0600

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,6 @@
 module.exports = {
-    outputDir: "~@/../server/public",
-    devServer: {
-        headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": "true" },
-        host: "0.0.0.0",
-        port: 8080,
-        hotOnly: false
-    },
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `
-        @import "~@/scss/_common.scss";
-        `
-            }
-        }
-    }
-};
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vue/'
+      : '/'
+  }
+  
